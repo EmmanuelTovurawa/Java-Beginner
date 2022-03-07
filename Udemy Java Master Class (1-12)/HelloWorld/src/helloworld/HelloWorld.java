c8722d40ddef1837f206f19a12940e26f16f4de0
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-03-07 21:54:28 +0200

    Project 4

---
+++ Udemy Java Master Class (1-12)/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+/**
+ *
+ * @author Manex
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+    
+}
