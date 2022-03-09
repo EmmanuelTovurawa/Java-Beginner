58a701895a42c4775e7e81a811eaa57de8b1f2b9
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-03-09 21:57:42 +0200

    Java Beginner

---
+++ Udemy Java Master Class (1-12)/Project4/src/Session3/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Session3;
+
+/**
+ *
+ * @author Manex
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+    
+}
