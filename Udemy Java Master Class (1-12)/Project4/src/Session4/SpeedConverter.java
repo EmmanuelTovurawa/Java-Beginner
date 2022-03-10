f06132d4302d10de9865260746001281c9562ee3
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-03-10 21:48:46 +0200

    Java Beginner

---
+++ Udemy Java Master Class (1-12)/Project4/src/Session4/SpeedConverter.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Session4;
+
+/**
+ *
+ * @author Manex
+ */
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour
+                    + " km/h= " + milesPerHour
+                    + " mi/h");
+
+        }
+    }
+
+}
---
+++ Udemy Java Master Class (1-12)/Project4/src/Session4/Statements.java
@@ -0,0 +1,29 @@
+package Session4;
+
+/**
+ *
+ * @author Manex
+ */
+public class Statements {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is"
+                + " another"
+                + " still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+    }
+}
