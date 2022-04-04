b54f4004945fd97f1b6ac271e51068509d009d96
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-04-04 15:57:37 +0200

    Java Beginner

---
+++ labs/14-Exceptions/Practices/Practice1/14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+/**
+ *
+ * @author Manex
+ */
+public class PlayerDatabaseException extends Exception {
+
+    public PlayerDatabaseException() {
+    }
+
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+}
