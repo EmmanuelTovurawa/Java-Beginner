f848e7fd2a1ca456a6b15ea462fea9f1a80bafd4
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-04-05 21:02:57 +0200

    Java Basics

---
+++ Udemy Java Master Class (1-12)/Project4/src/Section8/array1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Section8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Manex
+ */
+public class array1 {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i +", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The average is " + getAverage(myIntegers));
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for(int i=0; i< array.length; i++) {
+            sum += array[i];
+        }
+
+        return (double) sum / (double)array.length;
+
+    }
+    
+}
