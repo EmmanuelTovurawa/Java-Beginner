6c7538d97b68eb62fe1382d6a0acb16a0275ae63
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-04-09 09:53:33 +0200

    Java Beginner

---
+++ Udemy Java Master Class (1-12)/Project4/src/Section8LinkedLists/Customer.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Section8LinkedLists;
+
+/**
+ *
+ * @author Manex
+ */
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+
