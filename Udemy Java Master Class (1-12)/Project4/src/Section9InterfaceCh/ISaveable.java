2625d8a9d71ef2c45b265166aa64fde7bd457ea0
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-04-19 22:23:26 +0200

    java Basics

---
+++ Udemy Java Master Class (1-12)/Project4/src/Section9InterfaceCh/ISaveable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Section9InterfaceCh;
+
+import java.util.List;
+
+/**
+ *
+ * @author Manex
+ */
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+
+}
