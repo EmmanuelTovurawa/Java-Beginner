f961168376ff6945db6e7dacfe934da4a94bb4a9
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-06-06 21:05:38 +0200

    Java Beginners

---
+++ Udemy Java Master Class (1-12)/Project4/src/Section10GenericClassPt3/FootballPlayer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Section10GenericClassPt3;
+
+/**
+ *
+ * @author Manex
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
