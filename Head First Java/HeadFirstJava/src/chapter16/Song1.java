05ef7a9e4dc022aab244703174632af2e8790686
EmmanuelTovurawa <87187766+EmmanuelTovurawa@users.noreply.github.com>
2022-07-20 20:51:54 +0200

    Java Beginner

---
+++ Head First Java/HeadFirstJava/src/chapter16/Song1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter16;
+
+/**
+ *
+ * @author Manex
+ */
+public class Song1 {
+
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    public int compareTo(Song s) {
+        return title.compareTo(s.getTitle());
+    }
+
+    Song1(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
+
